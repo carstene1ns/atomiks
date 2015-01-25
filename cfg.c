bc073e902b0fd301089fecbe27b62bfed7c2fb3a
mv_fox <mv_fox@62db900f-6958-420d-9fd0-fefe20d3655f>
2015-01-25 11:15:03 +0000

    replaced my own config path routines by the SDL2 platform-agnostic getprefpath()
    git-svn-id: https://svn.code.sf.net/p/atomiks/code@30 62db900f-6958-420d-9fd0-fefe20d3655f

--- cfg.c
+++ cfg.c
@@ -1,10 +1,10 @@
 /*
  * This file is part of the Atomiks project
- * Copyright (C) Mateusz Viste 2013
- * 
+ * Copyright (C) Mateusz Viste 2013-2015
+ *
  * opens and provides a file descriptor to the
  * application's configuration file. multiplatform.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -19,26 +19,21 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <stdio.h>  /* FILE     */
-#include <stdlib.h> /* getenv(), NULL */
+#include <stdio.h>     /* FILE     */
+#include <stdlib.h>    /* NULL     */
+#include <SDL2/SDL.h>  /* SDL_GetPrefPath(), SDL_free() */
 
-/* mode is the fopen file mode to use (eg. "rb"). appname is the short name of your app (eg. "atomiks"). Returns an open FD ready to use, or NULL on failure */
+/* mode is the fopen file mode to use (eg. "rb"). appname is the short name
+ * of your app (eg. "atomiks"). Returns an open FD ready to use, or NULL on
+ * failure */
 FILE *cfg_fopen(char *mode, char *appname) {
-  char *tmpvar;
-  char filepath[4096], shortname[256];
+  char *prefpath;
+  char filepath[4096];
   FILE *fd;
   if ((mode == NULL) || (appname == NULL)) return(NULL);
-  tmpvar = getenv("APPDATA");  /* Windows */
-  if (tmpvar != NULL) {
-      snprintf(shortname, 256, "%s.cfg", appname);
-    } else {
-      tmpvar = getenv("HOME");  /* Linux/BSD */
-      if (tmpvar == NULL) return(NULL);
-      snprintf(shortname, 256, ".%s.conf", appname);
-  }
-
-  snprintf(filepath, 4096, "%s/%s", tmpvar, shortname);
+  prefpath = SDL_GetPrefPath("Mateusz Viste", appname);
+  snprintf(filepath, 4096, "%s/config.dat", prefpath);
+  SDL_free(prefpath);
   fd = fopen(filepath, mode);
-
   return(fd);
 }
